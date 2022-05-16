56a5c484dc6fc44b625f60e016aaa4c773630e86
Alexander-Aparicio <alexdel2022@gmail.com>
2022-05-16 10:48:51 -0500

    agregando atributo rel

--- components/proyectos/ProjectsComponents/WebsiteProject.js
+++ components/proyectos/ProjectsComponents/WebsiteProject.js
@@ -6,13 +6,13 @@ const WebsiteProject = ({...props})=>{
     return(
         <Repository>
 
-            <Project href={props.link} target='_blank'>
+            <Project href={props.link} target='_blank' rel="noreferrer">
                 <Web>{props.web}</Web>
                 <Technology color={props.color}><span>/</span>{props.tech}</Technology>
             </Project>
 
             <Card>
-                <a href={props.repo} target='_blank'>
+                <a href={props.repo} target='_blank' rel="noreferrer">
                     <Image src={Github} layout='intrinsic' alt="logo github"/>
                 </a>
             </Card>
