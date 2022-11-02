aac3f57e6871e1baa4a743cfbe70e1511e74f410
Alexander-Aparicio <alexdel2022@gmail.com>
2022-11-01 23:30:47 -0500

    add link social media

--- components/home/Professional.js
+++ components/home/Professional.js
@@ -8,21 +8,37 @@ import Twitter from "../../public/logos/twitter.svg";
 import Facebook from "../../public/logos/facebook.svg";
 import WhatsApp from "../../public/logos/whatsapp.svg";
 
-const Professional = ()=>{
-    return(
-        <Container>
-            <Card img={Alexander}/>
-            <Welcome>¡Bienvenido!</Welcome>
-            <SocialMedia 
-                redesSociales={[
-                    {name:'github', img: Github},
-                    {name:'twitter', img: Twitter},
-                    {name:'facebook', img: Facebook},
-                    {name:'whatsApp', img: WhatsApp},
-                ]}
-            />
-        </Container>
-    )
-}
+const Professional = () => {
+  return (
+    <Container>
+      <Card img={Alexander} />
+      <Welcome>¡Bienvenido!</Welcome>
+      <SocialMedia
+        redesSociales={[
+          {
+            name: "github",
+            img: Github,
+            link: "https://github.com/Alexander-Aparicio",
+          },
+          {
+            name: "twitter",
+            img: Twitter,
+            link: "https://twitter.com/Alexand66745116",
+          },
+          {
+            name: "facebook",
+            img: Facebook,
+            link: "https://www.facebook.com/alexander14mr2015/",
+          },
+          {
+            name: "whatsApp",
+            img: WhatsApp,
+            link: `https://api.whatsapp.com/send?phone=51924998093&text=Hola%20Alexander%20😃`,
+          },
+        ]}
+      />
+    </Container>
+  );
+};
 
-export default Professional
+export default Professional;
